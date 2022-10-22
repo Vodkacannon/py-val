2084d5271f35c4fd34e77bb0f607b27987da25ed
Armend Veseli <1509793+Vodkacannon@users.noreply.github.com>
2022-10-22 16:15:34 -0400

    Reformatting.

--- source/test.py
+++ source/test.py
@@ -3,11 +3,4 @@ import pyval as pv
 print(pv.math_eval(""))
 print(pv.math_eval("42"))
 print(pv.math_eval("(((1 + 2 + 3 - 6) / (4 * 5))**2.5"))
-print
-(
-  pv.math_eval
-  ("\
-  def malcious():
-    print("Some malicious code.")
-  ")
-)
+print(pv.math_eval("print('Some malicious code.'"))
