86830e778f26b5bd26e8ed56de8c689bf7992b7b
Armend Veseli <1509793+Vodkacannon@users.noreply.github.com>
2022-10-22 16:11:56 -0400

    Create test.py

---
+++ source/test.py
@@ -0,0 +1,13 @@
+import pyval as pv
+
+print(pv.math_eval(""))
+print(pv.math_eval("42"))
+print(pv.math_eval("(((1 + 2 + 3 - 6) / (4 * 5))**2.5"))
+print
+(
+  pv.math_eval
+  ("\
+  def malcious():
+    print("Some malicious code.")
+  ")
+)
