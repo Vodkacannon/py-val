ba6e68c18b1b16331592bf54ccfc5c07de56a945
Armend Veseli <1509793+Vodkacannon@users.noreply.github.com>
2022-10-23 16:32:47 -0400

    Update pyval.py

--- source/pyval.py
+++ source/pyval.py
@@ -1,15 +1,15 @@
-def eval_expression_of_x(x: str):
+def eval_expression_of_x(x: str): -> 
     # Block due to presence of some special characters.
     if any(sub_chr in [":", "="] for sub_chr in expression):
-        return 0
+        return None
 	
     # Block non-'x' letters.
     elif any((char.isalpha() || char != 'x') for char in expression) :
-        return 0
+        return None
 		
     # Block due to operator absence.
     elif not any(sub_str in ["+", "-", "*", "/", "%", "//", "**"] for sub_str in expression):
-        return 0
+        return None
 
     # Everything should be securely parsed.
     try:
@@ -21,19 +21,19 @@ def eval_expression_of_x(x: str):
 def eval_arithmetic(expression: str):
     # Block due to numeric absence.
     if not any(char.isdigit() for char in expression):
-        return 0
+        return None
     
     # Block due to presence of some special characters.
     elif any(sub_chr in [":", "="] for sub_chr in expression):
-        return 0
+        return None
 	
     # Block letters (upper and lower case).
     elif any(char.isalpha() for char in expression):
-        return 0
+        return None
 		
     # Block due to operator absence.
     elif not any(sub_str in ["+", "-", "*", "/", "%", "//", "**"] for sub_str in expression):
-        return 0
+        return None
 
     # Everything should be securely parsed.
     try:
