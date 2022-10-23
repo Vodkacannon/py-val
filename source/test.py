0251e294f16e6586db4106cf10946ed4efcc2d7c
Armend Veseli <1509793+Vodkacannon@users.noreply.github.com>
2022-10-23 16:27:36 -0400

    Update test.py

--- source/test.py
+++ source/test.py
@@ -1,6 +1,8 @@
 import pyval as pv
 
-print(pv.math_eval(""))
-print(pv.math_eval("42"))
-print(pv.math_eval("(((1 + 2 + 3 - 6) / (4 * 5))**2.5"))
-print(pv.math_eval("print('Some malicious code.'"))
+print(pv.eval_arithmetic(""))
+print(pv.eval_arithmetic("42"))
+print(pv.eval_arithmetic("(((1 + 2 + 3 - 6) / (4 * 5))**2.5"))
+print(pv.eval_arithmetic("print('Some malicious code.'"))
+
+print(pv.eval_expression_of_x(3.14159), "x * r**2")
