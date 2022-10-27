08e8864af7bfe18503d708d8eeec5d0b104fa6f6
Armend Veseli <1509793+Vodkacannon@users.noreply.github.com>
2022-10-27 16:39:33 -0400

    Update pyval.py

--- source/pyval.py
+++ source/pyval.py
@@ -1,10 +1,10 @@
-def eval_expression_of_x(x: str) -> float: 
+def eval_expression_of_x(expression: str) -> float: 
     # Block due to presence of some special characters.
     if any(sub_chr in [":", "="] for sub_chr in expression):
         return None
 	
     # Block non-'x' letters.
-    elif any((char.isalpha() || char != 'x') for char in expression) :
+    elif any((char.isalpha() || char != 'x') for char in expression):
         return None
 		
     # Block due to operator absence.
@@ -40,3 +40,24 @@ def eval_arithmetic(expression: str) -> float:
         return eval(expression)
     except:
         raise ArithmeticError("Invalid arithmetic.")
+
+	
+# Eval Integral.
+'''
+Float based iterator.
+'''
+def float_range(start: float, stop: float, increment: float) -> float:
+    while start < stop:
+        yield start
+        start += increment
+
+
+def riemann_sum_partial(python_expression: str, x: float, infinitesimal) -> float:
+    return eval_expression_of_x(python_expression) * infinitesimal
+
+
+'''
+Using a Riemann sum.
+'''
+def integrate(python_expression: str, low: float, high: float, infinitesimal: float = 0.001) -> float:
+    return sum(riemann_sum_partial(python_expression, x, infinitesimal) for x in float_range(low, high, infinitesimal))
